6b90a3fe28915f0d102a8148cad2cf5b5776acc1
Jhon Samuel Fernandez Gutierrez <jhon.fernandez.27@unsch.edu.pe>
2020-10-03 12:45:18 -0500

    Se agregó la clase interna NodoDoble dentro de ListaDoble.

--- src/main/java/com/unsch/collection/dinamicas/lineal/ListaDoble.java
+++ src/main/java/com/unsch/collection/dinamicas/lineal/ListaDoble.java
@@ -60,4 +60,36 @@ public class ListaDoble<Tipo> extends ListaSimple<Tipo> {
     public String toString() {
         return super.toString();
     }
+
+    /**
+     * Clase usada para moldear un nodo con doble enlace.
+     *
+     * @author jhon
+     **/
+    protected static class NodoDoble<Tipo> extends NodoSimple<Tipo> {
+
+        /** Almacena la referencia del nodo anterior */
+        private NodoDoble<Tipo> anterior;
+
+        /** Almacena la referencia del nodo siguiente */
+        private NodoDoble<Tipo> siguiente;
+
+        /**
+         * Este constructor inicia la referencia anterior y posterior del nodo, ademas inicia el valor del nodo.
+         *
+         * @param valor     este es el valor que se va a agregar a este nodo.
+         * @param anterior  referencia del anterior nodo
+         * @param siguiente referencia del siguiente nodo.
+         */
+        NodoDoble(NodoDoble<Tipo> anterior, Tipo valor, NodoDoble<Tipo> siguiente) {
+            super(valor, null);
+        }
+
+        /***/
+        @Override
+        public Tipo getValor() {
+            return super.getValor();
+        }
+    }
+
 }

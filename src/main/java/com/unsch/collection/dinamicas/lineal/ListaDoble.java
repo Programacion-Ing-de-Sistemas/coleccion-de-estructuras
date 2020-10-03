feec3f1e24898c7dc47952e3f17fd26ce9be87ee
Jhon Samuel Fernandez Gutierrez <jhon.fernandez.27@unsch.edu.pe>
2020-10-03 12:33:47 -0500

    Se agregó la clase ListaDoble.

---
+++ src/main/java/com/unsch/collection/dinamicas/lineal/ListaDoble.java
@@ -0,0 +1,63 @@
+package com.unsch.collection.dinamicas.lineal;
+
+public class ListaDoble<Tipo> extends ListaSimple<Tipo> {
+
+    public ListaDoble() {
+        super();
+    }
+
+    @Override
+    public void agregarInicio(Tipo item) {
+        super.agregarInicio(item);
+    }
+
+    @Override
+    public void agregarFinal(Tipo item) {
+        super.agregarFinal(item);
+    }
+
+    @Override
+    public void agregar(Tipo item) {
+        super.agregar(item);
+    }
+
+    @Override
+    public void remover(int posicion) {
+        super.remover(posicion);
+    }
+
+    @Override
+    public void removerInicio() {
+        super.removerInicio();
+    }
+
+    @Override
+    public void removerFinal() {
+        super.removerFinal();
+    }
+
+    @Override
+    public void remover(Tipo item) {
+        super.remover(item);
+    }
+
+    @Override
+    public void limpiar() {
+        super.limpiar();
+    }
+
+    @Override
+    public Tipo getItem(int posicion) {
+        return super.getItem(posicion);
+    }
+
+    @Override
+    public boolean existe(Tipo item) {
+        return super.existe(item);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}

65928273ab619326e8ea0a172298601b9bf7f1b8
Fernando <72231621+FernandoBosh@users.noreply.github.com>
2020-10-02 21:14:04 -0500

    Create ListaSimple.java
    se creo la clase  ListaSimple y se heredo  los metedos de la clase Lista.

---
+++ src/main/java/com/unsch/collection/dinamicas/lineal/ListaSimple.java
@@ -0,0 +1,39 @@
+package com.unsch.collection.dinamicas.lineal;
+
+
+
+/**
+ * Esta es una lista simplemente enlazada.
+ * @autor fernando
+ */
+public class ListaSimple <Tipo>  extends Lista<Tipo>{
+    @Override
+    public void agregar(Tipo item) {
+
+    }
+
+    @Override
+    public void remover(int posicion) {
+
+    }
+
+    @Override
+    public void remover(Tipo objeto) {
+
+    }
+
+    @Override
+    public void limpiar() {
+
+    }
+
+    @Override
+    public Tipo getItem(int posicion) {
+        return null;
+    }
+
+    @Override
+    public boolean existe(Tipo item) {
+        return false;
+    }
+}

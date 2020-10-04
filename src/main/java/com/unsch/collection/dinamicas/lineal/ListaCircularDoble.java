862d32f1bd24df5cf1dab0114bf80579d98dae7c
Jhon Samuel <jhon.fernandez.27@unsch.edu.pe>
2020-10-04 17:39:14 -0500

    ListaCicularDoble en progreso...
    Se va a implementar los metodos de la lista circular doble.

---
+++ src/main/java/com/unsch/collection/dinamicas/lineal/ListaCircularDoble.java
@@ -0,0 +1,104 @@
+package com.unsch.collection.dinamicas.lineal;
+
+/**
+ * Esta clase es usada para poder crear lista circulares con nodos de doble enlace.
+ *
+ * @author jhon
+ **/
+public class ListaCircularDoble<Tipo> extends ListaDoble<Tipo> {
+
+    /**
+     * empty
+     **/
+    public ListaCircularDoble() {
+        super();
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void agregarInicio(Tipo item) {
+        super.agregarInicio(item);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void agregarFinal(Tipo item) {
+        super.agregarFinal(item);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void agregar(Tipo item) {
+        super.agregar(item);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void remover(int posicion) {
+        super.remover(posicion);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void removerInicio() {
+        super.removerInicio();
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void removerFinal() {
+        super.removerFinal();
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void remover(Tipo item) {
+        super.remover(item);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public void limpiar() {
+        super.limpiar();
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public Tipo getItem(int posicion) {
+        return super.getItem(posicion);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public boolean existe(Tipo item) {
+        return super.existe(item);
+    }
+
+    /**
+     * empty
+     **/
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
